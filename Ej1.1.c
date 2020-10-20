3e778362161d9a9f022a85317d580cc2bb3943f7
Gualberto Asencio-Cortes <guaasecor@upo.es>
2020-10-20 12:12:19 +0000

    nuevos ejercicios

--- Ej1.1.c
+++ Ej1.1.c
@@ -1,6 +1,22 @@
 #include <stdio.h>
 
 void main () {
+  int hora;
+  int horas, minutos, segundos;
+  int aux;
 
+  /* Paso 1. Leer hora */
+  printf("Hora: ");
+  scanf("%d", &hora);
+
+  /* Paso 2. Convertir la hora a horas, minutos y segundos */
+  aux = hora;
+  segundos = aux % 60;
+  aux = aux / 60;
+  minutos = aux % 60;
+  horas = aux / 60;
+
+  /* Paso 3. Mostrar hora convertida */
+  printf("\nLa hora %d convertida da lugar a %d horas, % minutos y %d segundos.\n", hora, horas, minutos, segundos);
   
 }
