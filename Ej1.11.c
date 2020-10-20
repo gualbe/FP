a28aac61b57b2d39567bcd8a8292654efb0a6995
Gualberto Asencio-Cortes <guaasecor@upo.es>
2020-10-20 14:13:23 +0000

    nuevos ejercicios

---
+++ Ej1.11.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main () {
+  
+  int a, b, numero;
+
+  /* Paso 1. Inicializar secuencia aleatoria */
+  srand(time(NULL));
+
+  /* Paso 2. Pedir a y b */
+  printf("Introduzca a y b: ");
+  scanf("%d%d", &a, &b);
+
+  /* Paso 3. Obtener numero aleatorio */
+  numero = a + rand() % (b - a + 1);
+
+  /* Paso 4. Mostrar el numero aleatorio */
+  printf("\nNumero aleatorio: %d\n", numero);
+  
+  return 0;
+  
+}
